57da42674d6aa2de4269bfe7a05e65f938bfcca9
Marie P <map40j@gmail.com>
2023-01-22 16:20:35 -0800

    Final bug fixes

--- README.md
+++ README.md
@@ -3,15 +3,23 @@
 
 ## USER STORY
 ***
+```md
+AS A manager
+I WANT to generate a webpage that displays my team's basic info
+SO THAT I have quick access to their emails and GitHub profiles
+```
 
 ## HOW TO RUN THE APP
 ***
+* Open the terminal and type 'npm run test' to run the necessary tests. 
+* Once tests are completed type 'npm run start', the user will be prompted with a series of questions, after which the user will need to decide if the team mates are a manager, engineer or intern. Once the team input has been completed, select 'Create Team' from the drop down menu. 
+* The newly generated HTML (index.html) can be found in the folder 'Output'.
 
-## THE DEPLOYED REPO CAN BE FOUND HERE
+## VIDEO SUBMISSION
 ***
+Please click on the following link to view the video walk through:
 
-## INSTALLATION OF THE REPOSITORY
-***
+https://drive.google.com/file/d/1kkIpcJJ5iPR9tYoXx1ZQGe8t2Ogb2p9N/view
 
 ## IMAGES OF THE APP
 ***
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 //node modules
 const inquirer = require("inquirer");
 const fs = require("fs");
+const path = require("path");
+const outputDir = path.resolve(__dirname, "dist");
+const dist = path.join("dist", "index.html");
 
 //team member profiles
-const Manager=require("./lib/manager");
-const Engineer=require("./lib/engineer");
-const Intern=require("./lib/intern");
-const Employee=require("./lib/employee");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Employee = require("./lib/employee");
 
 // HTML generator
 const generateHTML = require("./src/generateHTML");
@@ -17,159 +20,138 @@ const teamMembers = [];
 
 // prompts
 const questions = async () => {
-   const answers = await inquirer
-   .prompt([
-/*const addmanager = () => {*/
+  const answers = await inquirer.prompt([
+    /*const addmanager = () => {*/
     /*return inquirer.prompt([*/
 
- {
-    type: 'input',
-    name: 'name',
-    message: 'What is your name?',
-    vaidate: nameInput => {
-      if (nameInput) {
-         return true;
-      }else{
-         console.log('Please enter your name')
-      }
-    }
- },
- {
-    type: 'input',
-    name: 'id',
-    message: 'What is your employee number?',
-    vaidate: idInput => {
-      if (idInput) {
-         return true;
-      }else{
-         console.log('Please enter your employee id number')
-      }
-    }
- },
- {
-    type: 'input',
-    name: 'email',
-    message: 'What is your email address?',
-    validate: emailInput => {
-    if (emailInput) {
-      return true;
-   }else{
-      console.log('Please enter your email address')
-   }
- }
-},
-
- {
-    type: 'input',
-    name: 'github',
-    message: 'What is your GitHub username?',
-    vaidate: githubInput => {
-      if (githubInput) {
-         return true;
-      }else{
-         console.log('Please enter your GitHub username')
-      }
-    }
- },
-
- {
-   type: 'list',
-   name: 'role',
-   message: 'What is your role on the team?',
-   choices: ['Manager', 'Engineer', 'Intern'],
-},
-])
-
- //Manager questions
- if(answers.role === "Manager") {
-   const managerAns = await inquirer
-   .prompt([
-
-   {
-      type: 'input',
-      name: 'officeNumber',
-      message: 'What is your Office Number?',
-      vaidate: officeNumberInput => {
-        if (officeNumberInput) {
-           return true;
-        }else{
-           console.log('Please enter your office Number')
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+      vaidate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name");
         }
       },
-   }
-])
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is your employee number?",
+      validate: (idInput) => {
+        if (idInput) {
+          return true;
+        } else {
+          console.log("Please enter your employee id number");
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?",
+      validate: (emailInput) => {
+        if (emailInput) {
+          return true;
+        } else {
+          console.log("Please enter your email address");
+        }
+      },
+    },
 
-   const newManager = new Manager (
-      answers. name,
-      answers. id,
-      answers. email,
-      managerAns. officeNumberInput
-   );
-  newTeamMemberData.push(newManager);
+    {
+      type: "list",
+      name: "role",
+      message: "What is your role on the team?",
+      choices: ["Manager", "Engineer", "Intern"],
+    },
+  ]);
 
-} else if (answers.role === "Engineer") {
-   const githubAns = await inquirer
-     .prompt ([
-   {
-      type: 'input',
-      name: 'github',
-      message: 'What is your github username?',
-   }
-])
-    const newEngineer = new Engineer ()
-     /*answers. name,
-     answers. id,
-     answers. email,
-     githubAns. github
-    );*/
-   newTeamMemberData.push(newEngineer);
+  //Manager questions
+  if (answers.role === "Manager") {
+    const managerAns = await inquirer.prompt([
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is your Office Number?",
+        validate: (officeNumberInput) => {
+          if (officeNumberInput) {
+            return true;
+          } else {
+            console.log("Please enter your office Number");
+          }
+        },
+      },
+    ]);
 
-} else if (answers.role === "Intern") {
-   const internAns = await inquirer
-     .prompt ([
-   {
-      type: 'input',
-      name: 'school',
-      message: 'What school did you attend?',
-   }
-])
-    const newIntern = new Intern ()
-     /*answers: name,
-     answers: id,
-     answers: email,
-     githubAns: github
-   };*/
-   newTeamMemberData.push(newIntern);
- 
+    const newManager = new Manager(
+      answers.name,
+      answers.id,
+      answers.email,
+      managerAns.officeNumber
+    );
 
+    teamMembers.push(newManager);
+  } else if (answers.role === "Engineer") {
+    const githubAns = await inquirer.prompt([
+      {
+        type: "input",
+        name: "github",
+        message: "What is your github username?",
+      },
+    ]);
+    const newEngineer = new Engineer(
+      answers.name,
+      answers.id,
+      answers.email,
+      githubAns.github
+    );
+    teamMembers.push(newEngineer);
+  } else if (answers.role === "Intern") {
+    const internAns = await inquirer.prompt([
+      {
+        type: "input",
+        name: "school",
+        message: "What school did you attend?",
+      },
+    ]);
+    const newIntern = new Intern(
+      answers.name,
+      answers.id,
+      answers.email,
+      internAns.school
+    );
+    teamMembers.push(newIntern);
+  }
 };
 
 async function promptQuestions() {
-   await questions()
+  await questions();
 
-const addMemeberAns = await inquirer
-   .prompt([
-   {
-     name:'addMember',
-     type: 'list',
-     choices: ['Add a new member', 'Create team'],
-     message: "Please choose your team member"
-   }
- ])
+  const addMemberAns = await inquirer.prompt([
+    {
+      name: "addMember",
+      type: "list",
+      choices: ["Add a new member", "Create team"],
+      message: "Please choose your team member",
+    },
+  ]);
 
- if (addMemberAns.addMember === 'Add a new member') {
-   return promptQuestions()
- }
- return createTeam();
-}  
+  if (addMemberAns.addMember === "Add a new member") {
+    return promptQuestions();
+  }
+  return createTeam();
+}
 
 promptQuestions();
 
-function createTeam () {
-console.log("Newbies", newTeamMemberData)
-fs.writeFileSync(
- "./output/index.html",
- generateTeam(newTeamMemberData),
- "utf-8"
-);
-}}
+function createTeam() {
+  console.log("Newbies", teamMembers);
+  fs.writeFileSync(
+    "./output/index.html",
+    generateHTML(teamMembers),
+    "utf-8"
+  );
+}
